cd18e1c9f329d0132a3f244d6e1a9d60d2b1d7aa
João <you@example.com>
2017-11-14 19:11:09 -0200

    add-result ok

--- interfacer/cli/command/add-result.js
+++ interfacer/cli/command/add-result.js
@@ -13,55 +13,42 @@ module.exports = function (vorpal) {
     .action(function (args, callback) {
 
       let events = betcoin.eventsManager.getAllEvents()
+      let eventsAddresses = R.map(R.prop('id'), events)
 
       if (!events || events.length === 0) {
         logger.log(colors.red('Não há eventos para apostar.'))
         callback()
       } else {
 
-        let eventsWithoutResults = []
+        return this.prompt([{
+          type: 'list',
+          name: 'resultEvent',
+          message: 'De qual evento é o resultado?',
+          choices: eventsAddresses
+        }, {
+          type: 'list',
+          name: 'resultBetType',
+          message: 'Qual é o tipo do resultado?',
+          choices: ['winner']
+        }, {
+          type: 'input',
+          name: 'result',
+          message: 'Qual é o resutlado? ',
+        }], (result) => {
+          try {
 
-        R.forEach((event) => {
-          if (betcoin.resultsManager.getByEvent(event.id) == null) {
-            eventsWithoutResults.push(event.id)
+            let newResult = betcoin.blockchain.addTransaction(betcoin.resultsManager.createResult(result.resultEvent, result.resultBetType, result.result))
+
+            logger.log(colors.blue(`O resultado ${newResult.id} foi adicionado com sucesso!`))
+          } catch (ex) {
+            if (ex instanceof ArgumentError || ex instanceof TransactionAssertionError)
+              logger.log(colors.red(ex.message))
+            else
+              throw ex
           }
-        }, events)
 
-        if (!eventsWithoutResults || eventsWithoutResults.length == 0) {
-          logger.log(colors.red('Não há eventos sem resultados.'))
           callback()
-        } else {
-
-          return this.prompt([{
-            type: 'list',
-            name: 'resultEvent',
-            message: 'De qual evento é o resultado?',
-            choices: eventsWithoutResults
-          }, {
-            type: 'list',
-            name: 'resultBetType',
-            message: 'Qual é o tipo do resultado?',
-            choices: ['winner']
-          }, {
-            type: 'input',
-            name: 'result',
-            message: 'Qual é o resutlado? ',
-          }], (result) => {
-            try {
-
-              let newResult = betcoin.blockchain.addTransaction(betcoin.resultsManager.createResult(result.resultEvent, result.resultBetType, result.result))
-
-              logger.log(colors.blue(`O resultado ${newResult.id} foi adicionado com sucesso!`))
-            } catch (ex) {
-              if (ex instanceof ArgumentError || ex instanceof TransactionAssertionError)
-                logger.log(colors.red(ex.message))
-              else
-                throw ex
-            }
-
-            callback()
-          })
-        }
+        })
 
       }
     })
